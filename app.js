79bd2765b6d0d8c7a77de13e8ddad79a17fdef77
FebrianAditya <febrian.aditya@student.upi.edu>
2024-11-08 20:36:19 +0700

    add new code and migration prisma

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ if (process.env.NODE_ENV !== "production") {
     require("dotenv").config()
 }
 
+const PORT = process.env.PORT || 3000;
+
 const express = require('express');
-// const { PrismaClient } = require("@prisma/client")
-// const prisma = new PrismaClient();
+const { PrismaClient } = require("@prisma/client")
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
@@ -18,18 +20,16 @@ app.get("/coba", (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { email, name, password } = req.body;
-  // try {
-  //   const user = await prisma.user.create({
-  //     data: { email, name, password },
-  //   });
-  //   res.status(201).json({ message: 'User registered successfully', user });
-  // } catch (error) {
-  //   res.status(400).json({ error: 'User registration failed', details: error.message });
-  // }
+  try {
+    const user = await prisma.user.create({
+      data: { email, name, password },
+    });
+    res.status(201).json({ message: 'User registered successfully', user });
+  } catch (error) {
+    res.status(400).json({ error: 'User registration failed', details: error.message });
+  }
 });
 
-// Start server
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`I LOVE YOU ${PORT}`);
